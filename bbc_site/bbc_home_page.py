2a42e18530d5a204397bcc372843ec85de56d4fe
kcorn1982 <kcorn1982@gmail.com>
2020-03-31 21:11:05 +0100

    update to get tests working with selenium

--- bbc_site/bbc_home_page.py
+++ bbc_site/bbc_home_page.py
@@ -1,5 +1,4 @@
 from selenium.webdriver.common.by import By
-from selenium import webdriver
 
 
 class BbcHomePage:
@@ -9,14 +8,7 @@ class BbcHomePage:
 
     def __init__(self, driver):
         self.driver = driver
-        driver.get(self.BBC_HOME_PAGE_URL)
 
     def go_to_home_page(self):
         self.driver.get(self.BBC_HOME_PAGE_URL)
 
-
-if __name__ == '__main__':
-    driver = webdriver.Chrome("../drivers/chromedriver")
-    driver.implicitly_wait(3)
-    x = BbcHomePage(driver)
-    x.go_to_home_page()
--- tests/test_bbc_home_page.py
+++ tests/test_bbc_home_page.py
@@ -1,15 +1,12 @@
 from selenium import webdriver
 from bbc_site.bbc_home_page import BbcHomePage
 
-driver = webdriver.Chrome("drivers/chromedriver")
-driver.implicitly_wait(3)
-
-BBC_HOME_PAGE = BbcHomePage(driver)
 
 class TestBbcHomePage:
 
-    def __init__(self):
-        self.BBC_HOME_PAGE = BbcHomePage()
+    driver = webdriver.Chrome("drivers/chromedriver")
+    driver.implicitly_wait(3)
+    BBC_HOME_PAGE = BbcHomePage(driver)
 
-    def go_to_home_page():
-        BBC_HOME_PAGE.go_to_home_page()
+    def test_go_to_home_page(self):
+        self.BBC_HOME_PAGE.go_to_home_page()
